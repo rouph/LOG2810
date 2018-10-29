1ef88668125e5d0f18a9e25e62657ce34a7312e5
rouph <elierouph@gmail.com>
2018-10-28 21:33:24 -0400

    added first version of the menu

--- TP1/log2810/Source.cpp
+++ TP1/log2810/Source.cpp
@@ -1,7 +1,17 @@
 #include "graph.h"
 #include <iostream>
-int main() 
+#include <string>
+
+int main()
 {
+
+	char option = 'e';
+	int start = 0;
+	int finish = 0;
+	string vehicule = "Ni-MH";
+	string patientRisk = "---";
+	string file = "centresLocaux1.txt";
+
 	graph test;
 	test.CreateGraph();
 	test.getShortestPath(1, 2);
@@ -15,5 +25,65 @@ int main()
 	test.getShortestPath(1, 1);
 	std::cout << endl;
 	test.getShortestPath(3, 5);
+
+
+
+	//INTERFACE
+
+
+
+	std::cout << " (a) Update map " << endl
+		<< " (b) Determine shortest safest path" << endl
+		<< " (c) Extract sub-graph " << endl
+		<< " (d) Quit" << endl;
+	cin >> option;
+
+	if (option == 'a' || option == 'A') {
+
+		std::cout << "Enter the file name of your map" << endl;
+		cin >> file;
+		graph graphe;
+		graphe.CreateGraph();
+		// REINITIALISER B ET C
+	}
+
+	if (option == 'b' || option == 'B') {
+		std::cout << "Select a starting point" << endl;
+		cin >> start;
+
+		std::cout << "Select a destination " << endl;
+		cin >> finish;
+
+		std::cout << "Enter the patient's health risk " << endl
+			<< " 1 : low risk " << endl
+			<< " 2 : medium risk " << endl
+			<< " 3 : high risk " << endl;
+		cin >> patientRisk;
+
+		//AJOUTER FONCTION QUI CHERCHE LE PLUS COURT CHEMIN
+	}
+
+	if (option == 'c' || option == 'C') {
+		std::cout << "Select a starting point" << endl;
+		cin >> start;
+
+		std::cout << "Enter the number of the chosen vehicule" << endl
+			<< "1 : Ni-MH" << endl
+			<< "2 : Li-ion" << endl;
+		cin >> vehicule;
+
+		std::cout << "Enter the patient's health risk " << endl
+			<< " 1 : low risk " << endl
+			<< " 2 : medium risk " << endl
+			<< " 3 : high risk " << endl;
+		cin >> patientRisk;
+
+		//AJOUTER FONCTION QUI FAI LE SOUS GRAPHE
+	}
+
+
+	if (option == 'd' || option == 'D') {
+
+	}
 	return 0;
 }
